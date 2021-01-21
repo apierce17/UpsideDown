739a06c583f370a2a635ebda846ed34683952644
Ashton Pierce <coalpierce@gmail.com>
2021-01-20 18:10:43 -0600

    fixed mobile carousel images, added carousel buttons and various smaller bugs

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -14,7 +14,7 @@
 
     // Collapse Navbar
     var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 200) {
+        if ($("#mainNav").offset().top > 100) {
             $("#mainNav").addClass("navbar-shrink");
         } else {
             $("#mainNav").removeClass("navbar-shrink");
