a434e4ebdd246619ebcb16be0a426f14384755cb
Ashton Pierce <coalpierce@gmail.com>
2021-10-12 10:29:31 -0500

    added required to contact form and checkbox to prevent spam, fixed nav menu in mobile not closing when unfocused or after selection, removed desktop navbar shrinking and going transparent, fixed issue of user able to scroll the background when item pop up is active, updated various things in TOS and Privacy Policy.

--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -6,22 +6,15 @@
         $(".navbar-collapse").collapse("hide");
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#mainNav",
-        offset: 74,
+    $(window).scroll(function () {
+        $(".navbar-collapse").collapse("hide");
     });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
-        } else {
-            $("#mainNav").removeClass("navbar-shrink");
-        }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav"
+    });
+
+
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
